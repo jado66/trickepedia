c428319b838a66475f504c9ae9c0ec4a84522953
jado66 <jado66@gmail.com>
2025-11-10 18:54:04 -0700

    Removed all gym management stuff

--- components/gym-management/waivers/field-components/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all field components for easy importing
-export { TextSectionField } from "./text-section-field";
-export { HealthHistoryField } from "./health-history-field";
-export { EmbeddedMediaField } from "./embedded-media-field";
-export { MultiSelectField } from "./multi-select-field";
-export { RadioGroupField } from "./radio-group-field";
-export { FileUploadField } from "./file-upload-field";
